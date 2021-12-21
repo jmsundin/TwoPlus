e2618bd772aed2ea9f5e751e3e254c1209c6b82c
Jon Sundin <jsundin91@gmail.com>
2021-12-21 09:38:22 -0800

    first commit beso app

---
+++ app/src/main/java/com/sundin/beso/ui/map/MapViewModel.kt
@@ -0,0 +1,13 @@
+package com.sundin.beso.ui.map
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MapViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is the Map Fragment"
+    }
+    val text: LiveData<String> = _text
+}
